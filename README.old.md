7dee2d5e92a975ffc268d7dea06594bf1564ff5f
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-12-29 16:53:32 +0200

    header

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# what time is it right now.com
+ a website that tells you the time right now. inspired by todd from bojack horseman
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+import HeaderImg from '../res/header.png';
+
+export default function Header(props) {
+    return(
+        <img className='header-img' alt="what time is it right now" src={HeaderImg}></img>
+    );
+}
